bb4073cb599a3f769f551ace6d949f26245f49af
Alex Ayerdi <AlexanderAyerdi2015@u.northwestern.edu>
2015-03-17 15:08:41 -0500

    yellow

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ window.onload = function ()
 {
 	var divider = document.getElementById("katiedivider");
 	divider.innerHTML = "this is my divider";
+
+	var yellows = document.getElementsByClass("yellow");
+
+	yellows[0].style.color = "yellow";
+
+	yellows[1].style.color = "yellow";
 }
